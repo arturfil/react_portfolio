829136f727e2d8b6249af87e2aa0a5c150a4d78d
Arturo Filio Villa <arturfil@hotmail.com>
2018-10-16 18:46:59 -0400

    changed landing page

--- react_portfolio_client/src/components/landing/Landing.jsx
+++ react_portfolio_client/src/components/landing/Landing.jsx
@@ -11,7 +11,9 @@ class Landing extends Component {
             <div className="row">
               <div className="col-md-12 text-center">
                 <h2 className="mt-landing h2-landing display-3 mb-4">
-                  Arturo Filio | Software Engineer
+                  Arturo Filio
+                  <br />
+                  Software Engineer
                 </h2>
                 <p className="lead"> It's all in the detail</p>
               </div>
